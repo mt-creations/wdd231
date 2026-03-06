d19bc9ba539031e2ab1f0e312c8bbf15f5d03dee
mt-creations <mthomas92551@gmail.com>
2026-03-06 10:47:20 -0800

    home page

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,5 @@
 
-
+/***************** NAVIGATION *******************/
 
 const navButton = document.querySelector('#ham-btn');
 const navBar = document.querySelector('#nav-bar');
@@ -7,13 +7,48 @@ const navBar = document.querySelector('#nav-bar');
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
     navBar.classList.toggle('show');
-  });
+    });
 
-/******************* FOOTER ******************************/
+/******************* FOOTER *********************/
+
 const currentYear = new Date().getFullYear();
 
-document.getElementById("currentYear").textContent = `©️${currentYear} 🌴 Michael Thomas 🌴 California`;
+document.getElementById("currentYear").textContent = `\u00A9 ${currentYear} ~ Michael Thomas ~ California`;
 
 document.getElementById("lastModified").textContent = `Last modified: ${document.lastModified}`;
 
 
+/*************** COURSE BUTTONS ******************/
+
+function showCourses(group) {
+    const wdd = document.querySelector(".courses-wdd");
+    const cse = document.querySelector(".courses-cse")
+
+    if (group === "all") {
+        wdd.style.display = "block";
+        cse.style.display = "block";
+    }
+
+    if (group === "courses-wdd") {
+        wdd.style.display = "block";
+        cse.style.display = "none";
+    }
+
+    if (group === "courses-cse") {
+        wdd.style.display = "none";
+        cse.style.display = "block";
+    }
+
+    let total = 0;
+    const visibleCourses = document.querySelectorAll(".courses-wdd p, .courses-cse p");
+
+    visibleCourses.forEach(course => {
+        if (course.parentElement.style.display !== "none") {
+            total += Number(course.dataset.credits);
+        }
+    })
+
+    document.querySelector('#total-credits').textContent = "The total credits for courses listed above is: " + total;
+
+
+}
