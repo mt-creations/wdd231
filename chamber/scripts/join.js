f5751bf556ec06792d2020a9bb4eda7744082ca1
mt-creations <mthomas92551@gmail.com>
2026-05-30 23:11:05 -0700

    join

---
+++ chamber/scripts/join.js
@@ -0,0 +1,16 @@
+document.getElementById("timestamp").value = new Date().toLocaleString();
+
+
+
+const url = "data/levels.json";
+const container = document.querySelector("#memberLevel");
+
+function displayLevels(levels) {
+    levels.forEach(level => {
+        let card = document.createElement('section');
+        card.classList.add("card");
+
+        let 
+
+    });
+}
