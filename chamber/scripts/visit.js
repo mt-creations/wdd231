9b04e9f36e1e374a1465ed686477030bc26f56dd
mt-creations <mthomas92551@gmail.com>
2026-06-07 19:07:43 -0700

    discover

--- chamber/scripts/visit.js
+++ chamber/scripts/visit.js
@@ -1,3 +1,29 @@
 
 
-const 
+const messageElement = document.querySelector('#visit');
+
+const lastVisit = localStorage.getItem('lastVisit');
+const now = Date.now();
+
+let message = '';
+
+if (!lastVisit) {
+    message = 'Welcome! Let us know if you have any questions.';
+} else {
+    const milSecsBetweenVisits = now - Number(lastVisit);
+    const daysBetweenVisits = Math.floor(
+        milSecsBetweenVisits / (1000 * 60 * 60 * 24)
+    );
+
+    if (daysBetweenVisits < 1) {
+        message = 'Back so soon! Awesome!';
+    } else if (daysBetweenVisits === 1) {
+        message = 'You last visited 1 day ago';
+    } else {
+        message = `You last visited ${daysBetweenVisits} days ago.`;
+    }
+}
+
+messageElement.textContent = message;
+
+localStorage.setItem('lastVisit', now);
